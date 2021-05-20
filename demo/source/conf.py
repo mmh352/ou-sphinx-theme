2acc6f2cfe0e76055253d2cef3476a6394d03759
Mark Hall <mark.hall@open.ac.uk>
2021-05-20 15:07:52 +0100

    v0.11.1

--- demo/source/conf.py
+++ demo/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020-2021, Mark Hall'
 author = 'Mark Hall'
 
 # The full version, including alpha/beta/rc tags
-release = '0.11.0'
+release = '0.11.1'
 
 
 # -- General configuration ---------------------------------------------------
