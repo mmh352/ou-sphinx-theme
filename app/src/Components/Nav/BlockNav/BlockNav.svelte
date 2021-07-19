ad3dbe425e6acdd3e749eee8e63500cc2e06e814
Mark Hall <mark.hall@open.ac.uk>
2021-07-19 15:39:59 +0100

    Fix a layout bug in the nav sidebar

--- app/src/Components/Nav/BlockNav/BlockNav.svelte
+++ app/src/Components/Nav/BlockNav/BlockNav.svelte
@@ -16,7 +16,7 @@
     });
 </script>
 
-<nav class="col-start-1 col-end-2 lg:col-start-2 lg:col-end-3 row-start-3 row-end-4 lg:row-start-2 lg:row-end-4 flex flex-col lg:border-r-2 border-solid border-gray-200 py-2 pr-4">
+<nav class="col-start-1 col-end-2 lg:col-start-2 lg:col-end-3 row-start-3 row-end-4 lg:row-start-2 lg:row-end-4 flex flex-col lg:border-r-2 border-solid border-gray-200 py-2 pr-4 overflow-auto">
     <ul bind:this={menuRootElement}>
         {#if $withinBlockNav}
             <BlockNavItem item={$withinBlockNav}/>
