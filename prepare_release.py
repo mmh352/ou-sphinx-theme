f098cec43c8b60eb274bd1735373b78ab4e52510
Mark Hall <mark.hall@open.ac.uk>
2021-07-30 14:57:38 +0100

    v0.14.4

---
+++ prepare_release.py
@@ -0,0 +1,30 @@
+import re
+
+
+VERSION = '0.14.4'
+
+
+def readlines(filename: str) -> 'list[str]':
+    with open(filename) as in_f:
+        return in_f.readlines()
+
+
+def writelines(filename: str, lines: 'list[str]'):
+    with open(filename, 'w') as out_f:
+        out_f.write(''.join(lines))
+
+
+def update_version(filename: str, pattern: str, version: str):
+    def replace_version(line: str) -> str:
+        if re.match(pattern, line):
+            return re.sub('[0-9]+\.[0-9]+\.[0-9]+', version, line)
+        else:
+            return line
+
+    writelines(filename, map(replace_version, readlines(filename)))
+
+
+update_version('package.json', '^  "version": "[0-9]+\.[0-9]+\.[0-9]",$', VERSION)
+update_version('app/package.json', '^  "version": "[0-9]+\.[0-9]+\.[0-9]",$', VERSION)
+update_version('pyproject.toml', '^version = "[0-9]+\.[0-9]+\.[0-9]+"$',  VERSION)
+update_version('demo/source/conf.py', "^.*release = '[0-9]+\.[0-9]+\.[0-9]+'$", VERSION)
