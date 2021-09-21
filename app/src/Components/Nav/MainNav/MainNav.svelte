063acc3bd9003c24fb4a89fa733b58fc815474db
Mark Hall <mark.hall@open.ac.uk>
2021-09-21 20:50:12 +0100

    Implemented top-menu scrolling. Fixes #38

--- app/src/Components/Nav/MainNav/MainNav.svelte
+++ app/src/Components/Nav/MainNav/MainNav.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	import { tick } from 'svelte';
+	import { tick, onMount, onDestroy, afterUpdate } from 'svelte';
     import { derived } from 'svelte/store';
+	import { tweened } from 'svelte/motion';
 
     import { project, tutorial, staticUrl, isInJupyterHub } from '../../../store/data';
     import { breakpoint } from '../../../store/breakpoint';
@@ -9,6 +10,9 @@
 
     let showMenu = false;
     let menuList = null;
+    let menuScroll = false;
+    let menuScrollPosition = tweened(0);
+    let scrollMax = 0;
 
     const blocks = derived(
         tutorial,
@@ -48,6 +52,31 @@
             });
         }
     }
+
+    function showSize() {
+        if (menuList) {
+            menuScroll = menuList.parentElement.scrollWidth > menuList.parentElement.clientWidth;
+            scrollMax = menuList.parentElement.scrollWidth - menuList.parentElement.clientWidth;
+        }
+    }
+
+    const unsubscribe = menuScrollPosition.subscribe((position) => {
+        if (menuList) {
+            menuList.parentElement.scrollLeft = position;
+        }
+    });
+
+    function scrollMenu(direction: number) {
+        if (menuList) {
+            menuScrollPosition.update((value) => {
+                return Math.max(0, Math.min(scrollMax, value + direction));
+            });
+        }
+    }
+
+    onMount(showSize);
+    afterUpdate(showSize);
+    onDestroy(unsubscribe);
 </script>
 
 <nav class="col-start-1 col-end-4 row-start-1 row-end-2 border-b-2 border-solid boder-gray-200 lg:border-b-0 w-full">
@@ -61,11 +90,11 @@
                 {/if}
             </button>
         </li>
-        <li class="flex-shrink flex-grow">
+        <li class="flex-shrink flex-grow overflow-hidden">
             <ul bind:this={menuList} class="flex flex-col lg:flex-row lg:items-end">
                 {#if showMenu || $breakpoint >= 3 || !$hasCurrent}
                     <li>
-                        <span class="block flex-shrink flex-grow px-3 py-2 text-blue font-bold border-solid {showMenu ? 'border-b-1' : ''} lg:border-b-2 border-gray-200">{$project.name} {$project.release}</span>
+                        <span class="block flex-shrink flex-grow px-3 py-2 text-blue font-bold border-solid {showMenu ? 'border-b-1' : ''} lg:border-b-2 border-gray-200 whitespace-nowrap">{$project.name} {$project.release}</span>
                     </li>
                 {/if}
                 {#each $blocks as block}
@@ -78,6 +107,22 @@
                 {/if}
             </ul>
         </li>
+        {#if menuScroll}
+            <li aria-hidden="true" class="flex-0">
+                {#if $menuScrollPosition <= 0}
+                    <span class="block pl-3 pr-1 py-2 text-gray border-b-2 border-solid border-gray-200">&lt;</span>
+                {:else}
+                    <button on:click={() => { scrollMenu(-50); }} class="block pl-3 pr-1 py-2 text-blue hover:text-blue-400 focus:text-blue-400 border-b-2 border-solid border-gray-200 hover:border-blue focus:border-blue">&lt;</button>
+                {/if}
+            </li>
+            <li aria-hidden="true" class="flex-0">
+                {#if $menuScrollPosition >= scrollMax}
+                    <span class="block pl-1 pr-3 py-2 text-gray border-b-2 border-solid border-gray-200">&gt;</span>
+                {:else}
+                    <button on:click={() => { scrollMenu(50); }} class="block pl-1 pr-3 py-2 text-blue hover:text-blue-400 focus:text-blue-400 border-b-2 border-solid border-gray-200 hover:border-blue focus:border-blue">&gt;</button>
+                {/if}
+            </li>
+        {/if}
         <li class="flex-shrink-0">
             <a href="https://www.open.ac.uk" target="_blank" class="block px-3 py-2 text-blue hover:text-blue-400 focus:text-blue-400 {showMenu ? 'border-b-1' : ''} lg:border-b-2 border-solid border-gray-200 hover:border-blue focus:border-blue">
                 {#if $staticUrl}
